ebf168e18ff55bb53440c405e02c1d33e28daa8e
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-06-09 14:07:11 -0700

    CSS added to forms

--- src/components/Header/HeaderRegister.js
+++ src/components/Header/HeaderRegister.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Header.css"
 function Header() {
   return (
-    <nav className="navbar navbar-light bg-warning">
+    <nav id="navbarr" className="navbar navbar-light bg-warning">
       <div className="row col-12 d-flex justify-content-center text-white">
         <span className="h3">Register</span>
       </div>
